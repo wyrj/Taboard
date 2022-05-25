731a7f144a85ef8a67f6edde31717bcd2407d000
wyrj <yrj0103@gmail.com>
2022-05-25 22:54:29 +0800

    feat: simple login card

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,41 @@
 import type { NextPage } from 'next';
+import styles from '../styles/Home.module.scss';
+import { Button, Card, Input } from 'antd';
+import { ArrowRightOutlined } from '@ant-design/icons';
+import { useState } from 'react';
+import { useAppDispatch, useAppSelector } from '../store';
+import { getUserName, registerUser } from '../store/userSlice';
 
 const Home: NextPage = () => {
+  const name = useAppSelector(getUserName);
+  const [localName, setLocalName] = useState(name);
+  const dispatch = useAppDispatch();
+
+  const handleEnterName = () => {
+    dispatch(registerUser(localName));
+  };
+  const content = name
+    ?
+    <div>
+      <div>Hello { name }</div>
+    </div>
+    :
+    <div className={styles['login-content']}>
+      <Input
+        className={styles['name-input']}
+        placeholder="username"
+        value={localName}
+        onChange={(e) => setLocalName(e.target.value)}
+      />
+      <Button disabled={!localName} type="primary" shape="circle" icon={<ArrowRightOutlined />} onClick={handleEnterName} />
+    </div>;
+
   return (
-    <div>Hello</div>
+    <div className={styles.root} >
+      <Card className={styles['login-card']}>
+        { content }
+      </Card>
+    </div>
   );
 };
 
